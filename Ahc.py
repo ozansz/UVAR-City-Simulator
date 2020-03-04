9e502b38b4c4854284b63bbeedb3ffebd71c7504
WINS Laboratory <cengwins@gmail.com>
2020-03-03 23:33:57 -0500

    Initial ad hoc computing files added

--- Ahc.py
+++ Ahc.py
@@ -3,75 +3,85 @@ import queue
 from threading import Thread
 import datetime
 
-
 def singleton(cls):
-    instance = [None]
+  instance = [None]
 
-    def wrapper(*args, **kwargs):
-        if instance[0] is None:
-            instance[0] = cls( *args, **kwargs )
-        return instance[0]
+  def wrapper(*args, **kwargs):
+    if instance[0] is None:
+      instance[0] = cls(*args, **kwargs)
+    return instance[0]
 
-    return wrapper
-
+  return wrapper
 
 @singleton
 class ComponentRegistry():
-    components = {}
+  components = {}
 
-    def getComponentByInstance(self, instance):
-        listOfKeys = list()
-        listOfItems = self.components.items()
-        for item in listOfItems:
-            if item[1] == instance:
-                listOfKeys.append( item[0] )
-        return listOfKeys
+  def getComponentByInstance(self, instance):
+    listOfKeys = list()
+    listOfItems = self.components.items()
+    for item in listOfItems:
+      if item[1] == instance:
+        listOfKeys.append(item[0])
+    return listOfKeys
 
-    def addComponent(self, component):
-        key = component.componentname + str( component.componentinstancenumber )
-        self.components[key] = component
+  def addComponent(self, component):
+    key = component.componentname + str(component.componentinstancenumber)
+    self.components[key] = component
 
-    def getComponentByKey(self, componentname, componentinstancenumber):
-        key = component.componentname + str( component.componentinstancenumber )
-        return self.components[key]
+  def getComponentByKey(self, componentname, componentinstancenumber):
+    key = componentname + str(componentinstancenumber)
+    return self.components[key]
 
-    def linkComponents(self, up, down):
-        up.downout = down
-        down.upout = up
+  def linkComponents(self, up, down):
+    up.downout = down
+    down.upout = up
+
+  def linkComponentsbyKey(self, upcomponentname, upcomponentinstancenumber, downcomponentname, downcomponentinstancenumber):
+    upkey = upcomponentname + str(upcomponentinstancenumber)
+    downkey = downcomponentname + str(downcomponentinstancenumber)
+    up = self.components[upkey]
+    down = self.components[downkey]
+    up.downout = down
+    down.upout = up
 
 class Event:
-    def __init__(self, caller, event, content):
-        self.caller = caller
-        self.event = event
-        self.content = content
-        self.time = datetime.datetime.now()
+  def __init__(self, caller, event, content):
+    self.caller = caller
+    self.event = event
+    self.content = content
+    self.time = datetime.datetime.now()
 
+class GenericComponentModel(abc.ABC):
+  eventhandlers = {}
 
-class GenericComponentModel( abc.ABC ):
-    eventhandlers = {}
+  def __init__(self, componentname, componentinstancenumber, handlerdict={}, ports={}, num_worker_threads=1):
+    self.inputqueue = queue.Queue()
+    self.componentname = componentname
+    self.componentinstancenumber = componentinstancenumber
+    self.upout = None
+    self.downout = None
 
-    def __init__(self, componentname, componentinstancenumber, handlerdict={}, ports={}, num_worker_threads=1):
-        self.inputqueue = queue.Queue()
-        self.componentname = componentname
-        self.componentinstancenumber = componentinstancenumber
-        self.upout = None
-        self.downout = None
+    for ev in handlerdict:
+      self.eventhandlers[ev] = handlerdict[ev]
 
-        for ev in handlerdict:
-            self.eventhandlers[ev] = handlerdict[ev]
-        for i in range( num_worker_threads ):
-            t = Thread( target=self.worker )
-            t.daemon = True
-            t.start()
+    self.registry = ComponentRegistry()
+    self.registry.addComponent(self)
 
-    def worker(self):
-        while True:
-            workitem = self.inputqueue.get()
-            if workitem.event in self.eventhandlers:
-                self.eventhandlers[workitem.event]( self, eventobj=workitem )  # call the handler
-            else:
-                print( f"Event Handler: {workitem.event} is not implemented" )
-            self.inputqueue.task_done()
+    for i in range(num_worker_threads):
+      t = Thread(target=self.worker)
+      t.daemon = True
+      t.start()
 
-    def trigger_event(self, eventobj: Event):
-        self.inputqueue.put_nowait( eventobj )
+  def worker(self):
+    while True:
+      workitem = self.inputqueue.get()
+      if workitem.event in self.eventhandlers:
+        print(f"I am {self.eventhandlers[workitem.event]}: {workitem.caller.componentname} called me at {workitem.time}")
+        self.eventhandlers[workitem.event](self, eventobj=workitem)  # call the handler
+      else:
+        print(f"Event Handler: {workitem.event} is not implemented")
+      self.inputqueue.task_done()
+
+  def trigger_event(self, eventobj: Event):
+    self.inputqueue.put_nowait(eventobj)
--- test.py
+++ test.py
@@ -3,70 +3,73 @@ from Ahc import Event
 from Ahc import ComponentRegistry
 import threading
 
+class Component(GenericComponentModel):
+  def onMessage(self, eventobj: Event):
+    myevent = Event(self, "message", "content")
+    if self.upout:
+      self.upout.trigger_event(myevent)
+    if self.downout:
+      self.downout.trigger_event(myevent)
 
-class Component( GenericComponentModel ):
-    def onMessage(self, eventobj: Event):
-        print( f"I am {self.componentname}: {eventobj.caller.componentname} called me at {eventobj.time}" )
-        myevent = Event( self, "message", "content" )
-        if self.upout:
-            self.upout.trigger_event( myevent )
-        if self.downout:
-            self.downout.trigger_event( myevent )
+  def onTimerExpired(self, eventobj: Event):
+    pass
 
-    def onTimerExpired(self, eventobj: Event):
-        print( f"I am {self.componentname}:event {eventobj.event} happened at {eventobj.time}" )
+  handlerdict = {
+    "message": onMessage,
+    "timerexpired": onTimerExpired
+  }
 
-    handlerdict = {
-        "message": onMessage,
-        "timerexpired": onTimerExpired
-    }
-
-    def __init__(self, componentname, componentid):
-        super().__init__( componentname, componentid, self.handlerdict )
-
+  def __init__(self, componentname, componentid):
+    super().__init__(componentname, componentid, self.handlerdict)
 
 registry = ComponentRegistry()
 
+class CompositeComponent(GenericComponentModel):
 
-class CompositeComponent( GenericComponentModel ):
+  def onMessage(self, eventobj: Event):
+    myevent = Event(self, "internalmessage", "content")
+    if self.upout:
+      self.upout.trigger_event(myevent)
+    if self.downout:
+      self.downout.trigger_event(myevent)
 
-    def onMessage(self, eventobj: Event):
-        print( f"I am {self.componentname}: {eventobj.caller.componentname} called me at {eventobj.time}" )
-        myevent = Event( self, "message", "content" )
-        if self.upout:
-            self.upout.trigger_event( myevent )
-#        if self.downout:
-#            self.downout.trigger_event( myevent )
+  def onInternalMessage(self, eventobj: Event):
+    print(f"I am {self.componentname}: {eventobj.caller.componentname} called me at {eventobj.time}")
+    myevent = Event(self, "message", "content")
+    if self.upout:
+      self.upout.trigger_event(myevent)
+    if self.downout:
+      self.downout.trigger_event(myevent)
 
-    handlerdict = {
-        "message": onMessage
-    }
+  handlerdict = {
+    "message": onMessage,
+    "internalmessage": onInternalMessage
+  }
 
-    def __init__(self, componentname, componentid):
-        self.linklayer = Component( "LinkLayer", 1 )
-        registry.addComponent( self.linklayer )
-        self.netlayer = Component( "NetworkLayer", 2 )
-        registry.addComponent( self.netlayer )
-        registry.linkComponents( self.linklayer, self.netlayer )
+  def __init__(self, componentname, componentid):
+    self.appllayer = Component("ApplicationLayer", componentid)
+    self.netlayer = Component("NetworkLayer", componentid)
+    self.linklayer = Component("LinkLayer", componentid)
 
-        super().__init__( componentname, componentid, self.handlerdict )
+    registry.linkComponents(self.appllayer,self.netlayer)
+    registry.linkComponents(self.netlayer,self.linklayer)
+    # registry.linkComponentsbyKey("NetworkLayer",componentid, "LinkLayer",componentid)
 
-
-
+    super().__init__(componentname, componentid, self.handlerdict)
 
 def Main():
-    cc = CompositeComponent("CompositeNodeComponent", 1)
-    myevent = Event( cc, "message", "content" )
-    cc.trigger_event( myevent )
+  cc = CompositeComponent("CompositeNodeComponent", 1)
 
-    registry.linkComponents(cc, cc.linklayer)
+  myevent = Event(cc, "message", "content")
+  cc.trigger_event(myevent)
 
-    print( registry.getComponentByInstance( cc.linklayer ) )
-    print( registry.getComponentByInstance( cc.netlayer ) )
-    print( registry.getComponentByInstance( cc ) )
+  registry.linkComponents(cc, cc.linklayer)
 
-    while (True): pass
+  print(registry.getComponentByInstance(cc.linklayer))
+  print(registry.getComponentByInstance(cc.netlayer))
+  print(registry.getComponentByInstance(cc))
 
+  while (True): pass
 
 if __name__ == "__main__":
-    Main()
+  Main()
