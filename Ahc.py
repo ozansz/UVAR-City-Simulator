603e7faa203a59be921119b5e3db3df0890385cf
WINS Laboratory <cengwins@gmail.com>
2020-03-14 15:53:30 -0400

    Some TODO list added

--- Ahc.py
+++ Ahc.py
@@ -4,184 +4,203 @@ from threading import Thread
 import datetime
 from enum import Enum
 
+#################FAILURE ASSUMPTIONS
+# TODO: Correctness properties: Safety and liveness
+# TODO: Model crash-stop, omission, crash-recover and byzantine failure models to the components
+# TODO: Add storage (temporary, persistent) to crash-recover model
+# TODO: Channel failure models: lossy-link, fair-loss, stubborn links, perfect links (WHAT ELSE?), FIFO perfect
+# TODO: Logged perfect links (tolerance to crashes), authenticated perfect links
+# TODO: Broadcast channels? and their failure models? Collisions?
+# TODO: Properties of all models separately: e.g., Fair loss, finite duplication, no fabrication in fair-loss link model
+# TODO: Packets: loss, duplication, sequence change, windowing?,
+# TODO: Eventually (unbounded time) or bounded time for message delivery?
 
-class PortNames( Enum ):
-    DOWN = "PORTDOWN"
-    UP = "PORTUP"
 
+#############TIMING ASSUMPTIONS
+# TODO: Event handling time, message sending time, assumptions about clock (drift, skew, ...)
+# TODO: 1. Asynch,  2. Synch 3. Partial-synch 4. Timed asynch
+# TODO: Causal-order (happen before), total-order,
+# TODO: Causal-order algebra!!!
+# TODO: Implement logical clocks (Lamport clocks, vector clocks) in event handling loop
+
+###### AUTOMATA and EXECUTIONS
+# TODO: Let component model hande executions and chekcs on executions (which event, in which order, per process or per system, similarity of executions)
+
+
+##### VISUALIZATION
+# TODO: Space-time diagrams for events
+
+
+class PortNames(Enum):
+  DOWN = "PORTDOWN"
+  UP = "PORTUP"
 
 def singleton(cls):
-    instance = [None]
+  instance = [None]
 
-    def wrapper(*args, **kwargs):
-        if instance[0] is None:
-            instance[0] = cls( *args, **kwargs )
-        return instance[0]
+  def wrapper(*args, **kwargs):
+    if instance[0] is None:
+      instance[0] = cls(*args, **kwargs)
+    return instance[0]
 
-    return wrapper
-
+  return wrapper
 
 @singleton
 class ComponentRegistry():
-    components = {}
+  components = {}
 
-    def getComponentByInstance(self, instance):
-        listOfKeys = list()
-        listOfItems = self.components.items()
-        for item in listOfItems:
-            if item[1] == instance:
-                listOfKeys.append( item[0] )
-        return listOfKeys
+  def getComponentByInstance(self, instance):
+    listOfKeys = list()
+    listOfItems = self.components.items()
+    for item in listOfItems:
+      if item[1] == instance:
+        listOfKeys.append(item[0])
+    return listOfKeys
 
-    def addComponent(self, component):
-        key = component.componentname + str( component.componentinstancenumber )
-        self.components[key] = component
+  def addComponent(self, component):
+    key = component.componentname + str(component.componentinstancenumber)
+    self.components[key] = component
 
-    def getComponentByKey(self, componentname, componentinstancenumber):
-        key = componentname + str( componentinstancenumber )
-        return self.components[key]
+  def getComponentByKey(self, componentname, componentinstancenumber):
+    key = componentname + str(componentinstancenumber)
+    return self.components[key]
 
-    def printComponents(self):
-        for itemkey in self.components:
-            cmp = self.components[itemkey]
-            print( f"I am {cmp.componentname}.{cmp.componentinstancenumber}" )
-            for i in cmp.ports:
-                connectedcmp = cmp.ports[i]
-                print( f"\t{i} {connectedcmp.componentname}.{connectedcmp.componentinstancenumber}" )
-
+  def printComponents(self):
+    for itemkey in self.components:
+      cmp = self.components[itemkey]
+      print(f"I am {cmp.componentname}.{cmp.componentinstancenumber}")
+      for i in cmp.ports:
+        connectedcmp = cmp.ports[i]
+        print(f"\t{i} {connectedcmp.componentname}.{connectedcmp.componentinstancenumber}")
 
 class Event:
-    def __init__(self, caller, event, content):
-        self.caller = caller
-        self.event = event
-        self.content = content
-        self.time = datetime.datetime.now()
-
+  def __init__(self, caller, event, content):
+    self.caller = caller
+    self.event = event
+    self.content = content
+    self.time = datetime.datetime.now()
 
 class GenericComponentModel:
 
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
-    def __init__(self, componentname, componentinstancenumber, num_worker_threads=2):
-        self.inputqueue = queue.Queue()
-        self.componentname = componentname
-        self.componentinstancenumber = componentinstancenumber
+  def __init__(self, componentname, componentinstancenumber, num_worker_threads=2):
+    self.inputqueue = queue.Queue()
+    self.componentname = componentname
+    self.componentinstancenumber = componentinstancenumber
 
-        try:
-            if self.ports:
-                pass
-        except AttributeError:
-            self.ports = {}
+    try:
+      if self.ports:
+        pass
+    except AttributeError:
+      self.ports = {}
 
-        self.registry = ComponentRegistry()
-        self.registry.addComponent( self )
+    self.registry = ComponentRegistry()
+    self.registry.addComponent(self)
 
-        self.inputqueue.put_nowait( Event( self, "init", None ) )
+    self.inputqueue.put_nowait(Event(self, "init", None))
 
-        for i in range( num_worker_threads ):
-            t = Thread( target=self.worker )
-            t.daemon = True
-            t.start()
+    for i in range(num_worker_threads):
+      t = Thread(target=self.worker)
+      t.daemon = True
+      t.start()
 
-    def connectMeToComponent(self, name, component):
-        try:
-            self.ports[name] = component
-        except AttributeError:
-            self.ports = {}
-            self.ports[name] = component
+  def connectMeToComponent(self, name, component):
+    try:
+      self.ports[name] = component
+    except AttributeError:
+      self.ports = {}
+      self.ports[name] = component
 
-    def connectMeToChannel(self, name, channel):
-        try:
-            self.ports[name] = channel
-        except AttributeError:
-            self.ports = {}
-            self.ports[name] = channel
-        portnameforchannel = self.componentname + str( self.componentinstancenumber )
-        channel.connectMeToComponent( portnameforchannel, self )
+  def connectMeToChannel(self, name, channel):
+    try:
+      self.ports[name] = channel
+    except AttributeError:
+      self.ports = {}
+      self.ports[name] = channel
+    portnameforchannel = self.componentname + str(self.componentinstancenumber)
+    channel.connectMeToComponent(portnameforchannel, self)
 
-    def senddown(self, event: Event):
-        self.ports[PortNames.DOWN].trigger_event( event )
+  def senddown(self, event: Event):
+    self.ports[PortNames.DOWN].trigger_event(event)
 
-    def sendup(self, event: Event):
-        self.ports[PortNames.UP].trigger_event( event )
+  def sendup(self, event: Event):
+    self.ports[PortNames.UP].trigger_event(event)
 
-    def sendself(self, event: Event):
-        self.trigger_event( event )
+  def sendself(self, event: Event):
+    self.trigger_event(event)
 
-    def worker(self):
-        while True:
-            workitem = self.inputqueue.get()
-            if workitem.event in self.eventhandlers:
-                # print(
-                #    f"I am {self.eventhandlers[workitem.event]}: {workitem.caller.componentname} called me at {workitem.time}" )
-                self.eventhandlers[workitem.event]( self, eventobj=workitem )  # call the handler
-            else:
-                print( f"Event Handler: {workitem.event} is not implemented" )
-            self.inputqueue.task_done()
+  def worker(self):
+    while True:
+      workitem = self.inputqueue.get()
+      if workitem.event in self.eventhandlers:
+        # print(
+        #    f"I am {self.eventhandlers[workitem.event]}: {workitem.caller.componentname} called me at {workitem.time}" )
+        self.eventhandlers[workitem.event](self, eventobj=workitem)  # call the handler
+      else:
+        print(f"Event Handler: {workitem.event} is not implemented")
+      self.inputqueue.task_done()
 
-    def trigger_event(self, eventobj: Event):
-        self.inputqueue.put_nowait( eventobj )
+  def trigger_event(self, eventobj: Event):
+    self.inputqueue.put_nowait(eventobj)
 
+class GenericChannel(GenericComponentModel):
+  pass
 
-class GenericChannel( GenericComponentModel ):
-    pass
+class AHCChannelError(Exception):
+  pass
 
-class AHCChannelError( Exception ):
-    pass
+class P2PFIFOChannel(GenericChannel):
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
+  def connectMeToComponent(self, name, component):
+    try:
+      self.ports[name] = component
+      print(f"Number of nodes connected: {len(self.ports)}")
+      if len(self.ports) > 2:
+        raise AHCChannelError("More than two nodes cannot connect to a P2PFIFOChannel")
+    except AttributeError:
+      self.ports = {}
+      self.ports[name] = component
+    # except AHCChannelError as e:
+    #    print( f"{e}" )
 
-class P2PFIFOChannel( GenericChannel ):
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
+  def onMessage(self, eventobj: Event):
+    callername = eventobj.caller.componentname + str(eventobj.caller.componentinstancenumber)
+    for item in self.ports:
+      callee = self.ports[item]
+      calleename = self.ports[item].componentname + str(self.ports[item].componentinstancenumber)
+      # print(f"I am connected to {calleename}. Will check if I have to distribute it to {item}")
+      if calleename == callername:
+        pass
+      else:
+        myevent = Event(self, "messagefromchannel", eventobj.content)
+        callee.trigger_event(myevent)
 
-    def connectMeToComponent(self, name, component):
-        try:
-            self.ports[name] = component
-            print( f"Number of nodes connected: {len( self.ports )}" )
-            if len( self.ports ) > 2:
-                raise AHCChannelError( "More than two nodes cannot connect to a P2PFIFOChannel" )
-        except AttributeError:
-            self.ports = {}
-            self.ports[name] = component
-        # except AHCChannelError as e:
-        #    print( f"{e}" )
+  eventhandlers = {
+    "init": onInit,
+    "message": onMessage
+  }
 
-    def onMessage(self, eventobj: Event):
-        callername = eventobj.caller.componentname + str( eventobj.caller.componentinstancenumber )
-        for item in self.ports:
-            callee = self.ports[item]
-            calleename = self.ports[item].componentname + str( self.ports[item].componentinstancenumber )
-            # print(f"I am connected to {calleename}. Will check if I have to distribute it to {item}")
-            if calleename == callername:
-                pass
-            else:
-                myevent = Event( self, "messagefromchannel", eventobj.content )
-                callee.trigger_event( myevent )
+class FIFOBroadcastChannel(GenericChannel):
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
-    eventhandlers = {
-        "init": onInit,
-        "message": onMessage
-    }
+  def onMessage(self, eventobj: Event):
+    callername = eventobj.caller.componentname + str(eventobj.caller.componentinstancenumber)
+    for item in self.ports:
+      callee = self.ports[item]
+      calleename = self.ports[item].componentname + str(self.ports[item].componentinstancenumber)
+      # print(f"I am connected to {calleename}. Will check if I have to distribute it to {item}")
+      if calleename == callername:
+        pass
+      else:
+        myevent = Event(self, "messagefromchannel", eventobj.content)
+        callee.trigger_event(myevent)
 
-
-class FIFOBroadcastChannel( GenericChannel ):
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
-
-    def onMessage(self, eventobj: Event):
-        callername = eventobj.caller.componentname + str( eventobj.caller.componentinstancenumber )
-        for item in self.ports:
-            callee = self.ports[item]
-            calleename = self.ports[item].componentname + str( self.ports[item].componentinstancenumber )
-            # print(f"I am connected to {calleename}. Will check if I have to distribute it to {item}")
-            if calleename == callername:
-                pass
-            else:
-                myevent = Event( self, "messagefromchannel", eventobj.content )
-                callee.trigger_event( myevent )
-
-    eventhandlers = {
-        "init": onInit,
-        "message": onMessage
-    }
+  eventhandlers = {
+    "init": onInit,
+    "message": onMessage
+  }
--- test.py
+++ test.py
@@ -4,152 +4,144 @@ import threading
 
 registry = ComponentRegistry()
 
-class ApplicationLayerComponent( GenericComponentModel ):
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
-        proposedval = random.randint(0,100)
-        randval = random.randint(0,1)
-        if randval == 0:
-            newmsg = MessageContent( proposedval, self.componentinstancenumber )
-            randdelay = random.randint(0,5)
-            time.sleep(randdelay)
-            self.sendself(Event(self,"propose", newmsg))
-        else:
-            pass
+class ApplicationLayerComponent(GenericComponentModel):
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
+    proposedval = random.randint(0, 100)
+    randval = random.randint(0, 1)
+    if randval == 0:
+      newmsg = MessageContent(proposedval, self.componentinstancenumber)
+      randdelay = random.randint(0, 5)
+      time.sleep(randdelay)
+      self.sendself(Event(self, "propose", newmsg))
+    else:
+      pass
 
-    def onMessageFromBottom(self, eventobj: Event):
-        print( f"{self.componentname}.{self.componentinstancenumber}: Gotton message {eventobj.content.value} from {eventobj.content.mynodeid}" )
-        # value = eventobj.content.value
-        # value += 1
-        # newmsg = MessageContent( value )
-        # myevent = Event( self, "agree", newmsg )
-        # self.trigger_event(myevent)
+  def onMessageFromBottom(self, eventobj: Event):
+    print(
+      f"{self.componentname}.{self.componentinstancenumber}: Gotton message {eventobj.content.value} from {eventobj.content.mynodeid}")
+    # value = eventobj.content.value
+    # value += 1
+    # newmsg = MessageContent( value )
+    # myevent = Event( self, "agree", newmsg )
+    # self.trigger_event(myevent)
 
-    def onPropose(self, eventobj: Event):
-        self.senddown( Event( self, "messagefromtop", eventobj.content ) )
+  def onPropose(self, eventobj: Event):
+    self.senddown(Event(self, "messagefromtop", eventobj.content))
 
-    def onAgree(self, eventobj: Event):
-        print(f"Agreed on {eventobj.content}")
+  def onAgree(self, eventobj: Event):
+    print(f"Agreed on {eventobj.content}")
 
-    def onTimerExpired(self, eventobj: Event):
-        pass
+  def onTimerExpired(self, eventobj: Event):
+    pass
 
-    eventhandlers = {
-        "init": onInit,
-        "propose": onPropose,
-        "messagefrombottom": onMessageFromBottom,
-        "agree": onAgree,
-        "timerexpired": onTimerExpired
-    }
+  eventhandlers = {
+    "init": onInit,
+    "propose": onPropose,
+    "messagefrombottom": onMessageFromBottom,
+    "agree": onAgree,
+    "timerexpired": onTimerExpired
+  }
 
+class NetworkLayerComponent(GenericComponentModel):
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
+  def onMessageFromTop(self, eventobj: Event):
+    self.senddown(Event(self, "messagefromtop", eventobj.content))
 
-class NetworkLayerComponent( GenericComponentModel ):
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
+  def onMessageFromBottom(self, eventobj: Event):
+    self.sendup(Event(self, "messagefrombottom", eventobj.content))
 
-    def onMessageFromTop(self, eventobj: Event):
-        self.senddown(Event( self, "messagefromtop", eventobj.content ))
+  def onTimerExpired(self, eventobj: Event):
+    pass
 
-    def onMessageFromBottom(self, eventobj: Event):
-        self.sendup(Event( self, "messagefrombottom", eventobj.content ))
+  eventhandlers = {
+    "init": onInit,
+    "messagefromtop": onMessageFromTop,
+    "messagefrombottom": onMessageFromBottom,
+    "timerexpired": onTimerExpired
+  }
 
-    def onTimerExpired(self, eventobj: Event):
-        pass
+class LinkLayerComponent(GenericComponentModel):
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
-    eventhandlers = {
-        "init": onInit,
-        "messagefromtop": onMessageFromTop,
-        "messagefrombottom": onMessageFromBottom,
-        "timerexpired": onTimerExpired
-    }
+  def onMessageFromTop(self, eventobj: Event):
+    myevent = Event(self, "messagefromtop", eventobj.content)
+    self.ports[PortNames.DOWN].trigger_event(myevent)
 
+  def onMessageFromBottom(self, eventobj: Event):
+    myevent = Event(self, "messagefrombottom", eventobj.content)
+    self.ports[PortNames.UP].trigger_event(myevent)
 
+  def onTimerExpired(self, eventobj: Event):
+    pass
 
-class LinkLayerComponent( GenericComponentModel ):
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
+  eventhandlers = {
+    "init": onInit,
+    "messagefromtop": onMessageFromTop,
+    "messagefrombottom": onMessageFromBottom,
+    "timerexpired": onTimerExpired
+  }
 
-    def onMessageFromTop(self, eventobj: Event):
-        myevent = Event( self, "messagefromtop", eventobj.content )
-        self.ports[PortNames.DOWN].trigger_event( myevent )
+class AdHocNode(GenericComponentModel):
 
-    def onMessageFromBottom(self, eventobj: Event):
-        myevent = Event( self, "messagefrombottom", eventobj.content )
-        self.ports[PortNames.UP].trigger_event( myevent )
+  def onInit(self, eventobj: Event):
+    print(f"Initializing {self.componentname}.{self.componentinstancenumber}")
 
-    def onTimerExpired(self, eventobj: Event):
-        pass
+  def onMessageFromTop(self, eventobj: Event):
+    myevent = Event(self, "message", eventobj.content)
+    self.ports[PortNames.DOWN].trigger_event(myevent)
 
-    eventhandlers = {
-        "init": onInit,
-        "messagefromtop": onMessageFromTop,
-        "messagefrombottom": onMessageFromBottom,
-        "timerexpired": onTimerExpired
-    }
+  def onMessageFromChannel(self, eventobj: Event):
+    myevent = Event(self, "messagefrombottom", eventobj.content)
+    self.ports[PortNames.UP].trigger_event(myevent)
 
+  eventhandlers = {
+    "init": onInit,
+    "messagefromtop": onMessageFromTop,
+    "messagefromchannel": onMessageFromChannel
+  }
 
+  def __init__(self, componentname, componentid):
+    # SUBCOMPONENTS
+    self.appllayer = ApplicationLayerComponent("ApplicationLayer", componentid)
+    self.netlayer = NetworkLayerComponent("NetworkLayer", componentid)
+    self.linklayer = LinkLayerComponent("LinkLayer", componentid)
 
-class AdHocNode( GenericComponentModel ):
+    # CONNECTIONS AMONG SUBCOMPONENTS
+    self.appllayer.connectMeToComponent(PortNames.DOWN, self.netlayer)
+    self.netlayer.connectMeToComponent(PortNames.UP, self.appllayer)
+    self.netlayer.connectMeToComponent(PortNames.DOWN, self.linklayer)
+    self.linklayer.connectMeToComponent(PortNames.UP, self.netlayer)
 
-    def onInit(self, eventobj: Event):
-        print( f"Initializing {self.componentname}.{self.componentinstancenumber}" )
+    # Connect the bottom component to the composite component....
+    self.linklayer.connectMeToComponent(PortNames.DOWN, self)
+    self.connectMeToComponent(PortNames.UP, self.linklayer)
 
-
-    def onMessageFromTop(self, eventobj: Event):
-        myevent = Event( self, "message", eventobj.content )
-        self.ports[PortNames.DOWN].trigger_event( myevent )
-
-    def onMessageFromChannel(self, eventobj: Event):
-        myevent = Event( self, "messagefrombottom", eventobj.content )
-        self.ports[PortNames.UP].trigger_event( myevent )
-
-    eventhandlers = {
-        "init": onInit,
-        "messagefromtop": onMessageFromTop,
-        "messagefromchannel": onMessageFromChannel
-    }
-
-    def __init__(self, componentname, componentid):
-        # SUBCOMPONENTS
-        self.appllayer = ApplicationLayerComponent( "ApplicationLayer", componentid )
-        self.netlayer = NetworkLayerComponent( "NetworkLayer", componentid )
-        self.linklayer = LinkLayerComponent( "LinkLayer", componentid )
-
-        # CONNECTIONS AMONG SUBCOMPONENTS
-        self.appllayer.connectMeToComponent( PortNames.DOWN, self.netlayer )
-        self.netlayer.connectMeToComponent( PortNames.UP, self.appllayer )
-        self.netlayer.connectMeToComponent( PortNames.DOWN, self.linklayer )
-        self.linklayer.connectMeToComponent( PortNames.UP, self.netlayer )
-
-        # Connect the bottom component to the composite component....
-        self.linklayer.connectMeToComponent( PortNames.DOWN, self )
-        self.connectMeToComponent( PortNames.UP, self.linklayer )
-
-        super().__init__( componentname, componentid )
-
+    super().__init__(componentname, componentid)
 
 class MessageContent:
-    def __init__(self, value, mynodeid):
-        self.value = value
-        self.mynodeid = mynodeid
+  def __init__(self, value, mynodeid):
+    self.value = value
+    self.mynodeid = mynodeid
 
 def Main():
-    nodes = []
-    ch1 = FIFOBroadcastChannel( "FIFOBroadcastChannel", 1 )
-    for i in range( 10 ):
-        cc = AdHocNode( "Node", i )
-        nodes.append( cc )
-        cc.connectMeToChannel( PortNames.DOWN, ch1 )
+  nodes = []
+  ch1 = FIFOBroadcastChannel("FIFOBroadcastChannel", 1)
+  for i in range(10):
+    cc = AdHocNode("Node", i)
+    nodes.append(cc)
+    cc.connectMeToChannel(PortNames.DOWN, ch1)
 
-    # print(registry.getComponentByInstance(cc.linklayer))
-    # print(registry.getComponentByInstance(cc.netlayer))
-    # print(registry.getComponentByInstance(cc))
+  # print(registry.getComponentByInstance(cc.linklayer))
+  # print(registry.getComponentByInstance(cc.netlayer))
+  # print(registry.getComponentByInstance(cc))
 
-    registry.printComponents()
+  registry.printComponents()
 
-    while (True): pass
-
+  while (True): pass
 
 if __name__ == "__main__":
-    Main()
+  Main()
